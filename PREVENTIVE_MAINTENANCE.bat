50548682e7b8eb1c1a180db837d2d9df4600edac
Eschechola <lucas.eschechola@outlook.com>
2020-04-03 09:15:06 -0300

    Adicionando Programa

---
+++ PREVENTIVE_MAINTENANCE.bat
@@ -0,0 +1,30 @@
+@echo off
+ECHO =======================================================================================
+ECHO =
+ECHO = Script para limpeza e manutencaoo preventiva dos discos rigidos. By: Lucas Eschechola
+ECHO =
+ECHO =======================================================================================
+
+FOR %%i in (C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: X: Y: Z:) DO IF exist %%i (
+    ECHO Executando limpeza no disco %%i
+    %%i 
+    CHKDSK
+    SFC/SCANNOW
+)
+
+        
+ECHO Seu computador foi limpo com exito :)
+ECHO Se esse pequeno aplicativo foi útil pra você, visite www.eschechola.com.br e me ajude :)
+
+PAUSE
+
+START "" https://www.eschechola.com.br/
+START "" https://medium.com/@lucas.eschechola
+START "" https://github.com/Eschechola
+START "" https://www.linkedin.com/in/lucas-eschechola-769179166/
+
+PAUSE
+
+
+
+
