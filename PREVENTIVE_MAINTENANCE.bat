2528504950560617e417c826a50e007120321828
Eschechola <lucas.eschechola@outlook.com>
2020-04-20 11:20:04 -0300

    Forçar limpeza de disco adicionado

--- PREVENTIVE_MAINTENANCE.bat
+++ PREVENTIVE_MAINTENANCE.bat
@@ -7,11 +7,10 @@ ECHO ===========================================================================
 
 FOR %%i in (C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: X: Y: Z:) DO IF exist %%i (
     ECHO Executando limpeza no disco %%i
-    %%i 
-    CHKDSK
-    SFC/SCANNOW
+    CHKDSK /F %%i 
 )
 
+SFC/SCANNOW
         
 ECHO Seu computador foi limpo com exito :)
 ECHO Se esse pequeno aplicativo foi útil pra você, visite www.eschechola.com.br e me ajude :)
